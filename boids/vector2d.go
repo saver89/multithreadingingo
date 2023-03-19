c554900ab70bb51f610f6c93c356d324cc23e685
vladimir.nee <neelkim1989@gmail.com>
2023-03-19 23:38:00 +0500

    feat: finish boids

--- boids/vector2d.go
+++ boids/vector2d.go
@@ -2,46 +2,46 @@ package main
 
 import "math"
 
-type Vector2d struct {
+type Vector2D struct {
 	x float64
 	y float64
 }
 
-func (v1 Vector2d) Add(v2 Vector2d) Vector2d {
-	return Vector2d{v1.x + v2.x, v1.y + v2.y}
+func (v1 Vector2D) Add(v2 Vector2D) Vector2D {
+	return Vector2D{v1.x + v2.x, v1.y + v2.y}
 }
 
-func (v1 Vector2d) Subtract(v2 Vector2d) Vector2d {
-	return Vector2d{v1.x - v2.x, v1.y - v2.y}
+func (v1 Vector2D) Subtract(v2 Vector2D) Vector2D {
+	return Vector2D{v1.x - v2.x, v1.y - v2.y}
 }
 
-func (v1 Vector2d) Multiply(v2 Vector2d) Vector2d {
-	return Vector2d{v1.x * v2.x, v1.y * v2.y}
+func (v1 Vector2D) Multiply(v2 Vector2D) Vector2D {
+	return Vector2D{v1.x * v2.x, v1.y * v2.y}
 }
 
-func (v1 Vector2d) AddV(d float64) Vector2d {
-	return Vector2d{v1.x + d, v1.y + d}
+func (v1 Vector2D) AddV(d float64) Vector2D {
+	return Vector2D{v1.x + d, v1.y + d}
 }
 
-func (v1 Vector2d) SubtractV(d float64) Vector2d {
-	return Vector2d{v1.x - d, v1.y - d}
+func (v1 Vector2D) SubtractV(d float64) Vector2D {
+	return Vector2D{v1.x - d, v1.y - d}
 }
 
-func (v1 Vector2d) MultiplyV(d float64) Vector2d {
-	return Vector2d{v1.x * d, v1.y * d}
+func (v1 Vector2D) MultiplyV(d float64) Vector2D {
+	return Vector2D{v1.x * d, v1.y * d}
 }
 
-func (v1 Vector2d) DivisionV(d float64) Vector2d {
-	return Vector2d{v1.x / d, v1.y / d}
+func (v1 Vector2D) DivisionV(d float64) Vector2D {
+	return Vector2D{v1.x / d, v1.y / d}
 }
 
-func (v1 Vector2d) limit(lower, upper float64) Vector2d {
-	return Vector2d{
+func (v1 Vector2D) limit(lower, upper float64) Vector2D {
+	return Vector2D{
 		math.Max(math.Min(v1.x, upper), lower),
 		math.Max(math.Min(v1.y, upper), lower),
 	}
 }
 
-func (v1 Vector2d) Distance(v2 Vector2d) float64 {
+func (v1 Vector2D) Distance(v2 Vector2D) float64 {
 	return math.Sqrt(math.Pow(v1.x-v2.x, 2) - math.Pow(v1.y-v2.y, 2))
 }
